2467e53f6a0d73608933c0339115ccb1d14115e1
Patrickodida <patrick.o.odida@gmail.com>
2024-12-04 12:35:50 +0300

    Delete payment by paymentId

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -25,6 +25,85 @@ const createPayment = async (req, res)=>{
     }
 }
 
+// Get payment by paymentId
+const getPayment = async (req, res)=>{
+    try{
+        const { paymentId } = req.params;
+        // Ensure that paymentId is a valid integer
+        const payment = await Prisma.payment.findUnique({
+            where: {paymentId: parseInt(paymentId)}
+        })
+        if(!payment){
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Payment not found!"})
+        }
+        res.status(StatusCodes.OK).json({message: "Successfully retrieved payment!", payment:payment})
+    }catch(error){
+        console.error(error)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "Error retrieving payment!", error: error.message})
+    }
+}
+
+// Get all payments
+const getAllPayments = async (req, res)=>{
+    try{
+        // Check if the 'results' query parameter exists and its a validd number
+        const limit = req.query.results ? parseInt(req.query.results) : undefined;
+        const payment = await Prisma.payment.findMany({
+            // Apply limit if its defined
+            take: limit
+        })
+        res.status(StatusCodes.OK).json({message: "Successfully retrieved all payments!", payment:payment})
+    }catch(error){
+        console.error(error)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "Error retrieving all payments!", error: error.message})
+    }
+}
+
+// Update payment
+const updatePayment = async (req, res)=>{
+    try{
+        const { paymentId } = req.params;
+        const { orderId, userId, amount, status, paymentMethod, transactionId } = req.body;
+        // update payment
+        const payment = await Prisma.payment.update({
+            where: {paymentId: parseInt(paymentId)},
+            data: {
+                orderId,
+                userId,
+                amount,
+                status,
+                paymentMethod,
+                transactionId
+            }
+        })
+        res.status(StatusCodes.OK).json({message: "Successfully updated payment!", payment:payment})
+    }catch(error){
+        console.error(error)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "Error updating payment!", error: error.message})
+    }
+}
+
+// Delete payment
+const deletePayment = async (req, res)=>{
+    try{
+        const { paymentId } = req.params;
+        const payment = await Prisma.payment.delete({
+            where: {paymentId: parseInt(paymentId)}
+        })
+        res.status(StatusCodes.OK).json({message: "Successfully deleted payment!", payment: payment})
+    }catch(error){
+        console.error(error)
+        if(error.code === "P2025"){
+            res.status(StatusCodes.NOT_FOUND).json({message: "Payment not found!"})
+        }
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "Error deleting payment!", error: error.message})
+    }
+}
+
 module.exports = {
-    createPayment
+    createPayment,
+    getPayment,
+    getAllPayments,
+    updatePayment,
+    deletePayment
 }
--- Routes/paymentsRoute.js
+++ Routes/paymentsRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createPayment } = require('../Controllers/paymentController')
+const { createPayment, getPayment, getAllPayments, updatePayment, deletePayment } = require('../Controllers/paymentController')
 const validateToken = require('../Utils/validateToken')
 const validateRequest = require('../Utils/joi.validator')
 const { paymentSchema } = require('../Utils/joi.schemas')
@@ -8,5 +8,13 @@ const router = express.Router()
 
 // Create payment
 router.post('/register', validateRequest(paymentSchema), createPayment);
+// Get payment by paymentId
+router.get('/:paymentId', getPayment);
+// Get all payments
+router.get('/', getAllPayments);
+// Update payment
+router.put('/:paymentId', validateRequest(paymentSchema), updatePayment);
+// Delete payment
+router.delete('/:paymentId', deletePayment);
 
 module.exports = router;
